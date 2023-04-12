55202b25a0e7857ed4c6d6949a180ace84482269
Johnny Harbieh <jharbieh@microsoft.com>
2023-04-12 11:33:53 -0500

    updated program to include voice selection

--- Program.cs
+++ Program.cs
@@ -1,30 +1,76 @@
-﻿using System.Reflection.PortableExecutable;
+﻿using System.Collections;
+using System.Reflection.PortableExecutable;
 using System.Speech.Synthesis;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        SpeechSynthesizer synth = new SpeechSynthesizer();  
+        //  Get current directory
+        var root = Directory.GetCurrentDirectory();
 
-        // Configure the audio output.   
-        // synth.SetOutputToWaveFile();
+        //  File path
+        var path = String.Empty;
+
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        //  Configure the audio output.   
+        path = Path.Combine(root, "readout.wav");
+
+        //  Write out to audio file
+        synth.SetOutputToWaveFile(path);
+
+        //  Write out to default audio device
         synth.SetOutputToDefaultAudioDevice();  
-        
-        // Voices
-        foreach (var voice in synth.GetInstalledVoices()) {
-            Console.WriteLine(voice.VoiceInfo.Name);
 
+        //  Arraylist of voices
+        var voices = new List<InstalledVoice>();
+
+        //  List all available voices and allow user to select a voice.
+        foreach (var voice in synth.GetInstalledVoices()) {
+            
+            //  Add voice to arraylist
+            voices.Add(voice);
+
+            //  Display the voice's name.
+            //  Console.WriteLine("Voice Name: " + voice.VoiceInfo.Name);
+        };
+
+        for (int i = 0; i < voices.Count; i++)
+        {
             //  Microsoft David Desktop
-            //  Microsoft Zira Desktop
+            //  Microsoft Zira Desktop 
+
+            Console.WriteLine("[{0}]:{1}",i,voices[i].VoiceInfo.Name);
         }
 
-        synth.SelectVoice("Microsoft Zira Desktop");
+        //  print out an empty line
+        Console.WriteLine();
+
+        //  Prompt the user to select a voice
+        Console.WriteLine("Select a voice: ");
+
+        var voiceSelection = Console.ReadLine();
+
+        //  Switch to selected voice
+        switch (voiceSelection)
+        {
+            case "0":
+                synth.SelectVoice(voices[0].VoiceInfo.Name);
+                break;
+            case "1":
+                synth.SelectVoice(voices[1].VoiceInfo.Name);
+                break;
+            default:
+                synth.SelectVoice(voices[0].VoiceInfo.Name);
+                break;
+        }
+
+        //  Select a voice
+        //  synth.SelectVoice("Microsoft Zira Desktop");
 
         // Read from file
-        var root = Directory.GetCurrentDirectory();
-        var path = Path.Combine(root, "README.md");
-
+        path = Path.Combine(root, "README.md");
         string text = System.IO.File.ReadAllText(path);
 
         //  Speak from text
--- README.md
+++ README.md
@@ -5,5 +5,4 @@ This is an introduction on how to read text using Speech Synthesis.
 [Text to Speech in NET](https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/june/speech-text-to-speech-synthesis-in-net)  
 [Add a package to a Net Core project using CLI](https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-add-package)  
 [Speech Synthesis in NET Framework](https://docs.microsoft.com/en-us/dotnet/api/system.speech.synthesis.speechsynthesizer?view=netframework-4.7.2)  
-[Examples on how to use Speech Synthesis on Windows](https://docs.microsoft.com/en-us/previous-versions/office/developer/speech-technologies/dd167624(v%3Doffice.14))  
-[How to read a Text file](https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file)  
+[Examples on how to use Speech Synthesis on Windows](https://docs.microsoft.com/en-us/previous-versions/office/developer/speech-technologies/dd167624(v%3Doffice.14))
